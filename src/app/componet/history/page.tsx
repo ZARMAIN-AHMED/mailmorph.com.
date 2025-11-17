dded11330868adb826f8157a6f94b4d204046906
ZARMAIN-AHMED <160420162+ZARMAIN-AHMED@users.noreply.github.com>
2025-11-17 20:10:10 +0500

    Update page.tsx

--- src/app/componet/history/page.tsx
+++ src/app/componet/history/page.tsx
@@ -8,7 +8,7 @@ import RepliesUI from "../reply/page";
 import InboxPredictor from "../predictor/page";
 import { Menu, X,  } from "lucide-react";
 
-const API_BASE = process.env.NEXT_PUBLIC_API_URL;
+const API_BASE =  process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
 
 interface Email {
   to: string;
